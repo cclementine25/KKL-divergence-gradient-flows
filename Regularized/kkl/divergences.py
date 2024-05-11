a5e5c97eeff1a1ab945faab45bbb5dd1ae048eea
Clementine Chazal <clementinechazal@gmail.com>
2024-05-11 23:03:13 +0200

    rangement

--- Regularized/kkl/divergences.py
+++ Regularized/kkl/divergences.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.stats as scs
-import kernels as kl
+import kkl.kernels as kl
 
 
 ######## MMD ##########
@@ -69,52 +69,9 @@ def KKL(x,y,k,Packy,alpha):
     Trxy = np.trace(A @ W.T @ np.diag(Lz * log_ou_0(Lz)) @ W) #K @ Klog)
     return Trxx - Trxy 
 
-#(log_ou_0([Lz[t]])[0]
-#Wasserstein Gradient of KKL
 
-
-
-
+    
 def WGrad_KKL(x,y,k,dk,Packy,alpha,sigma):
-    n = len(x)
-    m = len(y)
-    
-    Kx = 1/n * k(x,x)
-    Lx,U = np.linalg.eig(Kx)
-    Lx = np.real(Lx)
-    U = np.real(U)
-    U = U.T
-    logLx = log_ou_0(Lx)
-    
-    Ky = Packy[0] 
-    Kxy = k(x,y)
-    
-    K = np.concatenate([np.concatenate([alpha * Kx, np.sqrt(alpha*(1-alpha)/(n*m)) *Kxy],axis = 1),np.concatenate([np.sqrt(alpha*(1-alpha)/(n*m)) *Kxy.transpose(),(1-alpha)* Ky],axis = 1)],axis = 0)
-    K = K 
-    Lz,W = np.linalg.eig(K)
-    Lz = np.real(Lz)
-    W = np.real(W)
-    W = W.transpose()
-    logLz = log_ou_0(Lz)
-    T = np.zeros((n,2))
-    for i in range(n):
-        U_w = 1/np.sqrt(n) * k(x[i][None,:],x)[0] 
-        DU_w = (x - x[i][None,:])/sigma**2 * U_w[:,None] # ONLY FOR THE GAUSSIAN KERNEL
-        #DU_w = 1/ np.sqrt(n) * dk(w[None,:],x)[0]
-        
-        V_w = np.concatenate([np.sqrt(alpha) * U_w,np.sqrt((1-alpha)/m) * k(x[i][None,:],y)[0]]) 
-        DV_w = (np.concatenate([x,y]) - x[i][None,:])/sigma**2 * V_w[:,None] #only for gaussian kernel
-        #DV_w = np.concatenate([np.sqrt(alpha) * DU_w,np.sqrt((1-alpha)/m) * dk(w[None,:],y)[0]]) #np.concatenate([[np.sqrt(alpha/n) * dk(w,x[i]) for i in range(n)],[np.sqrt((1-alpha)/m) * dk(w,y[j]) for j in range(m)]])
-        
-        VW = V_w @ W.T 
-        DVW = W @ DV_w
-        
-        Trx = 2 * U_w @ U.T @ np.diag(logLx/Lx)  @ U @ DU_w 
-        Trz = 2 * VW @ (np.diag(logLz/(Lz+1e-9)) + (logLz[:,None] - logLz[None,:]) / (Lz[:,None] - Lz[None,:] + np.identity(n+m) + 1e-9 * np.ones((n+m,n+m))) * (W[:, :n] @ W.T[:n,:]) + np.diag(np.linalg.norm(W[:,:n],axis =1)**2 / Lz)) @ DVW # + np.diag(np.linalg.norm(W[:,:n],axis =1)**2 / Lz) + ((logLz[:,None] - logLz[None,:]) / (Lz[:,None] - Lz[None,:] + np.identity(n+m)) * (W[:, :n] @ W.T[:n,:]))) @ DVW
-        T[i] = Trx - Trz
-    return  T #- Tr3 - Tr4 
-    
-def WGrad_KKL2(x,y,k,dk,Packy,alpha,sigma):
     n = len(x)
     m = len(y)
     
---
+++ Regularized/kkl/gradient_descent.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as scs
+import scipy.optimize as sco
+
+
+def gradient_descent(J,dJ,x0,h,eps,n_it_max):
+    x = x0 
+    grad = dJ(x)
+    J_val = J(x)
+    X = [x0]
+    i = 0 
+    liste_J = []
+    Grad = []
+    while np.linalg.norm(grad) > eps and i < n_it_max:
+        if i%10 == 0:
+            print("T = " + str(i))
+        Grad.append(np.linalg.norm(grad))
+        liste_J.append(J_val)
+        grad = dJ(x)
+        x = x - h * grad
+        X.append(x)
+        J_val = J(x)
+        i = i + 1
+    return np.array(X),liste_J,Grad
+        
